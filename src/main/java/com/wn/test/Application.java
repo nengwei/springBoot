53b7dc86d83e6b4291107664ed4aec3a93797553
neng.wei <1009002925@qq.com>
2017-05-07 10:31:03 +0800

    springBoot...

---
+++ src/main/java/com/wn/test/Application.java
@@ -0,0 +1,23 @@
+package com.wn.test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * springBoot启动程序
+ * @author neng.wei
+ *
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+};
