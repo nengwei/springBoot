e6bed783a281748a797f312cd4bb13260fe78ef1
neng.wei <1009002925@qq.com>
2017-09-27 10:34:41 +0800

    添加echarts

--- src/main/java/com/wn/controller/HelloController.java
+++ src/main/java/com/wn/controller/HelloController.java
@@ -4,31 +4,41 @@ import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
- *  视图模版
+ * 视图模版
+ * 
  * @author neng.wei
  *
  */
 @Controller
 @EnableAutoConfiguration
 public class HelloController {
-	
+
 	private Logger logger = Logger.getLogger(HelloController.class);
-	 
-    /*
-    *   http://localhost:8080/hello?name=cn.7player
-     */
- 
-    @RequestMapping("/hello")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        logger.info("hello");
-        model.addAttribute("name", name);
-        return "hello";
-    }
+
+	/*
+	 * http://localhost:8080/hello?name=cn.7player
+	 */
+
+	@RequestMapping("/hello")
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		logger.info("hello");
+		model.addAttribute("name", name);
+		return "hello";
+	}
+
+	/**
+	 * http://localhost:8080/echarts
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/echarts")
+	public String echarts() {
+		return "echarts";
+	}
 
 }
